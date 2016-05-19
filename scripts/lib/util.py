972f43572cc568e1b3cf402c0437758302aa80b6
Chen Shen <chenshen@instagram.com>
2016-05-18 19:23:39 -0700

    [Cassandra LegoCastle 5/N] build rpm for Cassandra
    Summary:
    This diff build Cassandra rpm in lego castle. To achieve that, following modification applied here:
    
      -  we copy necessary rpmbuild file into `scripts/rpmbuild/` folder, which includes
      -- `SOURCES/cassandra.sh`,  # Necessary cassandra evironments.
      -- `SOURCES/patch_redhat `  # Patch to cassandra configs to run on Centos.
      -- `SPECS/apache-cassandra.spec` # The spec file to build RPM.
      - we update the `scripts/build` and it's dependencies so that it will run `arc realclean; arc build; arc artifacts; rpmbuild -ba ...` with correct parameters
      -- specifically, we add python utils to generate ig version number, in form of `YYYYMMDD+git$REV`.
      -- we add option to `ant` command as well as `rpmbuild` command to respect the ig version and specific build path
      - Then we update the `FacebookDiffCreatedListener` so that we will upload the generated rpm file into everstore via legocastle.
    
    Test Plan:
    Sandcastle:
    The rpm is generated here:
    ```
     For artifact rpm, Legocastle uploaded 1 path(s) to Everstore with handle @everstore:GICWmADZlBymPCgGAAyBjHIAAAAAbnsIAAAA:
    ```
    
    Download and extract the rpm from everstore, successfully install this rpm in my dev container.
    
    Reviewers: fdeliege, #ig-cassandra, dgadling
    
    Reviewed By: dgadling
    
    Subscribers: trunkagent, dgadling
    
    Differential Revision: https://phabricator.intern.facebook.com/D3285017
    
    Tasks: 10793373
    
    Signature: t1:3285017:1463420189:9238118cf1012ad85f4da3971516cd7581504907

--- scripts/lib/util.py
+++ scripts/lib/util.py
@@ -6,7 +6,12 @@
 
 import logging
 import os.path
+import shutil
 import socket
+import subprocess
+import sys
+import time
+import xml.etree.ElementTree as ET
 
 try:
     socket.gethostbyname('fwdproxy')
@@ -16,3 +21,43 @@ except:
     USE_PROXY = False
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+
+
+def get_base_version():
+    """Get base.version from build.xml"""
+    build_xml = os.path.join(PROJECT_ROOT, 'build.xml')
+    root = ET.parse(build_xml).getroot()
+    for _property in root.findall('property'):
+        if _property.get('name') == 'base.version':
+            return _property.get('value')
+    raise KeyError('Can\'t find property with name `base.version` in build.xml.')
+
+
+def get_head_revision():
+    p = subprocess.Popen('git rev-parse --short HEAD',
+                         shell=True, cwd=os.path.join(PROJECT_ROOT),
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = p.communicate()
+    if p.returncode != 0:
+        raise subprocess.CalledProcessError(err)
+    return output.strip()
+
+
+def get_instagram_version():
+    base_version = get_base_version()
+    date = time.strftime('%Y%m%d')
+    head_revision = get_head_revision()
+    return '%s+git%s.%s' % (base_version, date, head_revision)
+
+
+def rpmbuild(artifact_file, version):
+    rpmbuild_dir = os.path.join(PROJECT_ROOT, 'scripts/rpmbuild')
+    shutil.copyfile(artifact_file,
+                    os.path.join(rpmbuild_dir, 'SOURCES', os.path.basename(artifact_file)))
+    subprocess.check_call(['rpmbuild',
+                           '-ba',
+                           '--define', '_topdir %s' % rpmbuild_dir,
+                           '--define', '_instagram_version %s' % version,
+                           'SPECS/apache-cassandra.spec'],
+                           cwd=rpmbuild_dir)
+    return os.path.join(rpmbuild_dir, 'RPMS/noarch/apache-cassandra-%s-1.noarch.rpm' % version)
