7c78f81468fc683edb296ae92f9266ae24559778
Aleksey Yeschenko <aleksey@yeschenko.com>
2017-09-21 14:29:05 +0100

    Implement short read protection on partition boundaries
    patch by Aleksey Yeschenko; reviewed by Sam Tunnicliffe for
    CASSANDRA-13595

--- src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
+++ src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
@@ -1102,6 +1102,11 @@ public class SinglePartitionReadCommand extends ReadCommand
              + ClusteringIndexFilter.serializer.serializedSize(clusteringIndexFilter(), version);
     }
 
+    public boolean isLimitedToOnePartition()
+    {
+        return true;
+    }
+
     /**
      * Groups multiple single partition read commands.
      */
